5fdb6eabbc4930c4a0184f2857b0aecf2aff7ec1
caoshenzhou <1791915772@qq.com>
2021-05-24 10:31:59 +0800

    添加了视频播放器模块

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,70 @@
 <template>
   <v-row>
-    <v-col v-for="n in 4" :key="n" cols="6">
-      <v-card height="400"></v-card>
+    <v-col cols="6">
+      <v-card>
+        <div class="player">
+          <video-player
+            ref="videoPlayer"
+            :playsinline="true"
+            :options="playerOptions"
+          ></video-player>
+        </div>
+      </v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-card>
+        <div class="player">
+          <video-player
+            ref="videoPlayer"
+            :playsinline="true"
+            :options="playerOptions1"
+          ></video-player>
+        </div>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+window.videojs = require("video.js");
+require("video.js/dist/lang/zh-CN");
+import "video.js/dist/video-js.css";
+import { videoPlayer } from "vue-video-player";
+
 export default {
   name: "Home",
 
-  data: () => ({}),
+  components: {
+    videoPlayer,
+  },
 
-  components: {},
+  data: () => ({
+    playerOptions: {
+      height: "360",
+      autoplay: true,
+      muted: true,
+      language: "zh-CN",
+      playbackRates: [0.7, 1.0, 1.5, 2.0],
+      sources: [
+        {
+          type: "video/mp4",
+          src: "http://localhost:8090/data/720(5).MP4",
+        },
+      ],
+    },
+    playerOptions1: {
+      height: "360",
+      autoplay: true,
+      muted: true,
+      language: "en",
+      playbackRates: [0.7, 1.0, 1.5, 2.0],
+      sources: [
+        {
+          type: "video/mp4",
+          src: "http://localhost:8090/data/720(2).MP4",
+        },
+      ],
+    },
+  }),
 };
 </script>
